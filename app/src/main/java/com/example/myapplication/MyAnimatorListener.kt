0bbbce4d313f60f76c9e84ef6c1fd7bca24f0fce
magicadong <932056657@qq.com>
2020-07-24 14:16:41 +0800

    创建和配置Activity 按钮事件监听 属性动画和动画事件监听 apply和also 多语言支持 横竖屏切换

---
+++ app/src/main/java/com/example/myapplication/MyAnimatorListener.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import android.animation.Animator
+
+open class MyAnimatorListener: Animator.AnimatorListener {
+    override fun onAnimationRepeat(p0: Animator?) {
+
+    }
+
+    override fun onAnimationEnd(p0: Animator?) {
+
+    }
+
+    override fun onAnimationCancel(p0: Animator?) {
+
+    }
+
+    override fun onAnimationStart(p0: Animator?) {
+
+    }
+}
